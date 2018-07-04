0c59564193a7afb169488eb95389d2e7dd186ce1
Gabriel Lourenço <gaabriiel.xd@gmail.com>
2018-07-03 22:14:50 -0300

    Implementação do clique no botão entrar

--- app/src/main/java/com/inovaufrpe/carropipa/LoginActivity.java
+++ app/src/main/java/com/inovaufrpe/carropipa/LoginActivity.java
@@ -1,13 +1,28 @@
 package com.inovaufrpe.carropipa;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+    private Button btnEntrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_login);
+        btnEntrar = findViewById(R.id.btnEntrar);
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(LoginActivity.this, HomeFisicaActivity.class);
+                finish();
+                startActivity(it);
+            }
+        });
     }
 }
