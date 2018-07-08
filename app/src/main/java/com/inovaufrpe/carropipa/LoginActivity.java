43f129a87f543fe8df407a6e1eedd4a9e3be95ec
Jorge Paulo <dev.jorge.ps@outlook.com>
2018-07-08 03:04:51 -0300

    Adicionado arquivo de rastreamento de localização em tempo real do firebase, adicionadas dependêncidas do gradle para isso e adicionadas necessidades de permissão do manifest

--- app/src/main/java/com/inovaufrpe/carropipa/LoginActivity.java
+++ app/src/main/java/com/inovaufrpe/carropipa/LoginActivity.java
@@ -1,14 +1,22 @@
 package com.inovaufrpe.carropipa;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Switch;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
     private Button btnEntrar;
+    private static final int NOTIFICATION_PERMISSION_CODE = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +27,45 @@ public class LoginActivity extends AppCompatActivity {
         btnEntrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent it = new Intent(LoginActivity.this, HomeFisicaActivity.class);
+                Intent it;
+                Switch swtMotorista = findViewById(R.id.switch1);
+                if (swtMotorista.isChecked()) {
+                    it = new Intent(LoginActivity.this, HomeCaminhoneiroActivity.class);
+                } else {
+                    it = new Intent(LoginActivity.this, HomeFisicaActivity.class);
+                }
                 finish();
                 startActivity(it);
             }
         });
+        requestNotificationPermission();
+    }
+
+    private void requestNotificationPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_NOTIFICATION_POLICY) == PackageManager.PERMISSION_GRANTED)
+            return;
+
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_NOTIFICATION_POLICY)) {
+
+        }
+
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_NOTIFICATION_POLICY}, NOTIFICATION_PERMISSION_CODE );
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        //Checking the request code of our request
+        if (requestCode == NOTIFICATION_PERMISSION_CODE ) {
+
+            //If permission is granted
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                //Displaying a toast
+                Toast.makeText(this, "Permission granted now you can read the storage", Toast.LENGTH_LONG).show();
+            } else {
+                //Displaying another toast if permission is not granted
+                Toast.makeText(this, "Oops you just denied the permission", Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
