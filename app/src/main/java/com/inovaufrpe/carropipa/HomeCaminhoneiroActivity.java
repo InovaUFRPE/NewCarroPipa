88305575a63b551137e4eda2bb5d0f0c9172579a
Gabriel Lourenço <gaabriiel.xd@gmail.com>
2018-08-06 20:15:03 -0300

    Adiciona dinheiro da pessoa na home

--- app/src/main/java/com/inovaufrpe/carropipa/HomeCaminhoneiroActivity.java
+++ app/src/main/java/com/inovaufrpe/carropipa/HomeCaminhoneiroActivity.java
@@ -37,6 +37,7 @@ public class HomeCaminhoneiroActivity extends AppCompatActivity {
     private PedidosAdapter adapter;
     private TextView olaUsuario;
     private TextView strSemServico;
+    private TextView tvDinheiro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +47,8 @@ public class HomeCaminhoneiroActivity extends AppCompatActivity {
         recyclerView = findViewById(R.id.recycle);
         olaUsuario = (TextView) findViewById(R.id.txtViewOla);
         strSemServico = findViewById(R.id.tvNenhumPedido);
+        tvDinheiro = findViewById(R.id.txtViewDinheiro);
+
 
         try {
             recuperaInformacoes();
@@ -88,6 +91,7 @@ public class HomeCaminhoneiroActivity extends AppCompatActivity {
                 try {
                     cliente = new JSONObject(result);
                     olaUsuario.setText("Olá, " + cliente.getString("nomerazaosocial"));
+                    tvDinheiro.setText("R$ "+ (int) cliente.getDouble("dinheiro") +",00");
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
